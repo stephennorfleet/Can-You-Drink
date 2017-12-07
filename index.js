c8d6e5b7cfe0e3733d6ff988c2e9c43b7fae3da6
Stephen Norfleet <30849573+stephennorfleet@users.noreply.github.com>
2017-12-07 12:46:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var age = 14;
+
+if (age > 21) {
+  //alert ("You can drink");
+} else {
+  //alert ("You can't drink...yet");
+}
+
+if (age > 21) {
+  alert ("You can drink because you are " + age);
+} else {
+  alert ("You can't drink yet because you are " + age);
+}
